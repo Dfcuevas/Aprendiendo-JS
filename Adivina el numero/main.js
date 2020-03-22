77e4608c7ae5e89e329061e2f4a4a8f37bfbfdc4
Diego Cuevas <ingdcuevas@gmail.com>
2020-03-22 09:38:18 -0500

    Documentando js

--- Adivina el numero/main.js
+++ Adivina el numero/main.js
@@ -9,3 +9,72 @@ const guessField = document.querySelector('.guessField');
 
 let guessCount = 1;
 let resetButton;
+guessField.focus();
+
+function checkGuess() {
+  let userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = 'Intentos anteriores: ';
+  }
+  guesses.textContent += userGuess + ' ';
+
+  if(userGuess === randomNumber) {
+    lastResult.textContent = '¡Felicidades! ¡Lo adivinaste!';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+  }else if(guessCount === 10) {
+    lastResult.textContent = '!!!¡¡¡Fin del juego¡¡¡!!!';
+    setGameOVer();
+  }else {
+    lastResult.textContent = '¡Equivocado!';
+    lastResult.style.backgroundColor = 'red';
+    if(userGuess < randomNumber) {
+      lowOrHi.textContent = '¡El numero es muy bajo!!';
+    }else if (userGuess > randomNumber) {
+      lowOrHi.textContent = '¡El numero es muy grande!';
+    }
+  }
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+}
+
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Iniciar nuevo juego';
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+  guessCount = 1;
+
+  let resetParas = document.querySelectorAll('.resultParas p');
+  for (let i = 0; i < resetParas.length; i++) {
+    resetParas[i].textContent = '';
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+
+  lastResult.style.backgroundColor = 'white';
+
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+
+//Concatenacion: En la programacion se le llama concatenar a unir cadenas de texto//
+//Inicio ejemplo//
+//let name = 'Bingo';
+//let hello = ' Saluda!';
+//let greeting = name + hello;
+//Final Ejemplo //
---
+++ Adivina el numero/mainVar.js
@@ -0,0 +1,25 @@
+let myName;
+myName = 'Diego';
+
+function logName () {
+  console.log(myName);
+}
+
+logName ();
+
+
+//------------------Arrays---------------//
+
+/*Un array es un objeto que contiene varios elementos encerrados entre corchetes y separador por comas. */
+
+let myNameArray = ['Chris', 'Bob', 'Jim'];
+
+
+//------------------Objetos---------------//
+
+/*Es una estructura de codigo que modela o representa a un objeto de la vida real. Puedes tener un objeto que represente a una caja que contenga datos sobre su ancho, largo y alto, o podrias tener un objeto que represente a una presona, y contenga datos sobre su nombre, altura, peso, que idioma habla, como saluda en este, y mas*/
+
+let dog = {
+  nombre: 'Blacky',
+  raza: 'Maluquin siberiano'
+}
